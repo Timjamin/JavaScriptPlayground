8556ea3b91357e3f12e737292d5a45b7cd15c772
Timjamin <156866843+Timjamin@users.noreply.github.com>
2026-09-22 10:48:31 -0500

    Completed all 5 challenges + bonuses

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,49 @@
 console.log("Script loaded!");
 
-
-function moveTheThing(){
-    // Your code here
+function moveTheThing() {
+    if (document.getElementById("leftBlock").innerHTML != "") {
+        document.getElementById("rightBlock").innerHTML =
+        document.getElementById("leftBlock").innerHTML;
+        document.getElementById("leftBlock").innerHTML = "";
+    } else {
+        document.getElementById("leftBlock").innerHTML =
+        document.getElementById("rightBlock").innerHTML;
+        document.getElementById("rightBlock").innerHTML = "";
+    }
 }
 
-function styleTheText(){
-    // Your code here
+function styleTheText() {
+    let styles = `
+    #fancyText {
+        font-size: 100px;
+        font-family: "Comic Sans MS", "Comic Sans", "Courier New", monospace;
+        color: blue;
+    }
+`;
+
+    let styleSheet = document.createElement("style");
+    styleSheet.textContent = styles;
+    document.head.appendChild(styleSheet);
 }
 
-function getFormValues(){
-    // Your code here
+function getFormValues() {
+    document.getElementById("firstResult").innerHTML = document.getElementById("firstField").value;
+    document.getElementById("secondResult").innerHTML = document.getElementById("secondField").value;
+    document.getElementById("thirdResult").innerHTML = document.getElementById("thirdField").checked;
 }
 
-function countTheStuff(){
-    // Your code here
+function countTheStuff() {
+    document.getElementById("countOfP").innerHTML = document.documentElement.innerHTML.split("</p>").length
+    document.getElementById("countOfH2").innerHTML = document.documentElement.innerHTML.split("</h2>").length
+    document.getElementById("countOfTD").innerHTML = document.documentElement.innerHTML.split("</td>").length
 }
 
-function addNewRow(){
-    // Your code here
+let rowCount = 3
+
+function addNewRow() {  
+    document.getElementById("addRowsTable").insertRow(-1).innerHTML = "<td>" + ++rowCount +"</td>"
 }
 
-function yourBonusChallenge(){
-    // Your code here
+function yourBonusChallenge() {
+    window.close();
 }
